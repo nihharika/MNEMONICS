a8327ed181334eb1b6a871df14fe4a1ec228b6d7
Shiva <shivaxwarrior@gmail.com>
2024-02-23 11:59:43 +0530

    add: login and register route

---
+++ src/database/db.ts
@@ -0,0 +1,23 @@
+// uses mongodb for connection
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || "mongodb://localhost:27017", {
+            dbName: "SecureUni",
+        });
+        const connection = mongoose.connection;
+
+        connection.on("connected", () => {
+            console.log("MongoDB connected successfully");
+        });
+
+        connection.on("error", (err) => {
+            console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+            process.exit();
+        });
+    } catch (error) {
+        console.log("Something went wrong!");
+        console.log(error);
+    }
+}
