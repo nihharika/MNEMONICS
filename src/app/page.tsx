9d3b985b6cd31c9269d2e7cb70439afdbf79ae90
Shiva <shivaxwarrior@gmail.com>
2024-02-23 17:13:51 +0530

    add: login and other page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import { Bug, CalendarCheck, List, Map } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
+    const homeItems = [
+        {
+            name: "Checklist",
+            link: "/checklist",
+            icon: List,
+        },
+        {
+            name: "Map",
+            link: "/emergency-call",
+            icon: Map,
+        },
+        {
+            name: "Report",
+            link: "/report",
+            icon: Bug,
+        },
+        {
+            name: "Events",
+            link: "/event",
+            icon: CalendarCheck,
+        },
+    ];
     return (
-        <main className="bg-black min-h-screen text-white">
-            <h1>Hello world</h1>
+        <main className=" mt-7">
+            <div className="bg-green-500 p-4 rounded-lg h-56 w-full"></div>
+            <div className="flex gap-8 justify-between items-center mt-7 border-b-2 border-black/ mb-2">
+                {homeItems.map((item, index) => (
+                    <Link href={item.link} key={index} className="flex-col justify-center items-center flex gap-3">
+                        <item.icon />
+                        <span className="text-md tracking-wide border-b-2 border-black/20">{item.name}</span>
+                    </Link>
+                ))}
+            </div>
         </main>
     );
 }
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,7 +1,73 @@
+"use client";
 import React from "react";
+import { BellRing, CircleUser, Home, Map, MessageCircleMore } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const navItems = [
+        {
+            name: "Home",
+            link: "/",
+            icon: Home,
+        },
+        {
+            name: "Chat",
+            link: "/chat",
+            icon: MessageCircleMore,
+        },
+        {
+            name: "Map",
+            link: "/emergency-call",
+            icon: Map,
+        },
+        {
+            name: "Alert",
+            link: "/live-chat",
+            icon: BellRing,
+        },
+        {
+            name: "Profile",
+            link: "/profile",
+            icon: CircleUser,
+        },
+    ];
+    const pathname = usePathname();
+    return (
+        <>
+            <nav className="flex justify-between">
+                <Link
+                    href={"/emergency-call"}
+                    className=" bg-[#009963] text-lg w-44
+             p-3 tracking-wide text-center h-full rounded-full text-white font-bold"
+                >
+                    Emergency Call
+                </Link>
+                <Link
+                    href={"/live-chat"}
+                    className=" bg-[#f5f0e5] w-32 text-center text-lg
+             p-3 rounded-full font-bold"
+                >
+                    Live Chat
+                </Link>
+            </nav>
+            <div className="w-full h-auto fixed bottom-0 right-0 left-0">
+                <div className="flex gap-2 justify-between flex-wrap border-t-2 border-black/10">
+                    {navItems.map((item, index) => {
+                        const isActive = (pathname.includes(item.link) && item.link.length > 1) || pathname === item.link;
+                        return (
+                            <Link href={item.link} key={index}>
+                                <span className="flex gap-2 flex-col items-center justify-center p-4">
+                                    <item.icon size={24} className={`${isActive && "fill-black"}`} />
+                                    <p>{item.name}</p>
+                                </span>
+                            </Link>
+                        );
+                    })}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Navbar;
